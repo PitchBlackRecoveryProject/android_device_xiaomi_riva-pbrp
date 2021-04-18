d3b0410281a008d829caaf1643ec3a0bbd147029
Erwin Abu Bakar Sidik <64487987+erwinabs@users.noreply.github.com>
2021-04-18 23:21:47 +0700

    Update omni_riva.mk

--- omni_riva.mk
+++ omni_riva.mk
@@ -15,7 +15,7 @@
 #
 
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/omni/config/gsm.mk)
+#$(call inherit-product, vendor/omni/config/gsm.mk)
 
 $(call inherit-product, device/xiaomi/riva/full_riva.mk)
 
